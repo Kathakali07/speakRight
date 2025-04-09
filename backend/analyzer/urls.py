1f83b547fa6c5c88d1667cc98ad74099dbf79dda
puitandysatyam <satyam287b@gmail.com>
2025-04-09 15:53:31 +0530

    satyam : cleanup files

--- backend/analyzer/urls.py
+++ backend/analyzer/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from analyzer.views.voice import transcribe_view
-#from analyzer.views.grammar import grammar_view
 
 urlpatterns = [
-    path('voice/', transcribe_view, name='transcribe'),
-    #path('grammar/', grammar_view, name='grammar_check'),
+    path('voice/', transcribe_view, name='transcribe')
 ]
--- backend/analyzer/views/grammar.py
+++
@@ -1,7 +0,0 @@
-'''from django.http import JsonResponse
-from analyzer.grammar_corrector.util import grammar_corrector
-
-def grammar_view(request):
-    sample_text = "This is an example with bad grammer."
-    result = check_grammar(sample_text)
-    return JsonResponse(result)'''
